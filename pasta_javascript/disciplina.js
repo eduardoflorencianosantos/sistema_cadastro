bcf58c68077f67a070543e05b570127eaf98ac4e
Eduardo Florenciano dos Santos <127545659+eduardoflorencianosantos@users.noreply.github.com>
2024-10-03 22:08:36 -0300

    Delete pasta_javascript directory

--- pasta_javascript/disciplina.js
+++
@@ -1,6 +0,0 @@
-// Função que faz o cadastro funcionar
-function disciplina() {
-    let nome_disciplina = document.getElementById('nome-disciplina').value;
-    let professor_disciplina = document.getElementById('professor-disciplina').value;
-    let turma = document.getElementById('turma').value;
-}
