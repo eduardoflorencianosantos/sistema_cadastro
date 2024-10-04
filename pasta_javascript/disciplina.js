35af172fa6139232cd5a02adf94a77001c3b7e13
Eduardo Florenciano dos Santos <127545659+eduardoflorencianosantos@users.noreply.github.com>
2024-10-03 22:06:11 -0300

    Add files via upload

---
+++ pasta_javascript/disciplina.js
@@ -0,0 +1,6 @@
+// Função que faz o cadastro funcionar
+function disciplina() {
+    let nome_disciplina = document.getElementById('nome-disciplina').value;
+    let professor_disciplina = document.getElementById('professor-disciplina').value;
+    let turma = document.getElementById('turma').value;
+}
---
+++ pasta_javascript/encaminhamento.js
@@ -0,0 +1,14 @@
+// Função que faz o cadastro funcionar
+function encaminhamento() {
+    let nome_aluno = document.getElementById('nome-aluno').value;
+    let nome_professor = document.getElementById('nome-professor').value;
+    let disciplina = document.getElementById('disciplina').value;
+    let turma = document.getElementById('turma').value;
+    let data_ocorrencia = document.getElementById('data-ocorrencia').value;
+    let horario_ocorrencia = document.getElementById('horario-ocorrencia').value;
+    let ocorrencia_pedagogica = document.getElementById('ocorrencia-pedagogica').value;
+    let ocorrencia_monitores = document.getElementById('ocorrencia-monitores').value;
+    let outro = document.getElementById('outro').value;
+    let quantidade_chamadas = document.getElementById('quantidade-chamadas').value;
+    let punicao = document.getElementById('punicao').value;
+}
