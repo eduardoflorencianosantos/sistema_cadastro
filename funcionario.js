13997f4032464505a5cd92834ccf1a6292eab9e5
Eduardo Florenciano dos Santos <127545659+eduardoflorencianosantos@users.noreply.github.com>
2024-10-03 22:15:48 -0300

    Add files via upload

---
+++ funcionario.js
@@ -0,0 +1,8 @@
+// Função que faz o cadastro funcionar
+function funcionario() {
+    let nome_funcionario = document.getElementById('nome-funcionario').value;
+    let cpf_funcionario = document.getElementById('cpf').value;
+    let telefone_funcionario = document.getElementById('telefone').value;
+    let setor = document.getElementById('setor').value;
+    let admissao = document.getElementById('admissao').value;
+}
---
+++ responsavel.js
@@ -0,0 +1,8 @@
+// Função que faz o cadastro funcionar
+function responsavel() {
+    let nome_responsavel = document.getElementById('nome-responsavel').value;
+    let cpf_responsavel = document.getElementById('cpf').value;
+    let nome_filho = document.getElementById('nome-filho').value;
+    let cpf_filho = document.getElementById('cpf-filho').value;
+    let telefone_responsavel = document.getElementById('telefone-responsavel').value;
+}
