cd6b3b8b93cca0870ec312f8c7476544ad533bb6
Eduardo Florenciano dos Santos <127545659+eduardoflorencianosantos@users.noreply.github.com>
2024-10-16 21:14:27 -0300

    Add files via upload

---
+++ frequencia.js
@@ -0,0 +1,10 @@
+// Função que faz o cadastro funcionar
+function frequencia() {
+    let nome_aluno = document.getElementById('nome-aluno').value;
+    let responsavel_busca = document.getElementById('responsavel-busca').value;
+    let contato_responsavel = document.getElementById('contato-responsavel').value;
+    let turma = document.getElementById('turma').value;
+    let busca_ativa = document.getElementById('busca-ativa').value;
+    let meio_contato = document.getElementById('meio-contato').value;
+    let resultado_contato = document.getElementById('resultado-contato').value;
+}
