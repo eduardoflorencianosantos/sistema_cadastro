283f09d1754b6049479159a9aa622971d1248d63
Eduardo Florenciano dos Santos <127545659+eduardoflorencianosantos@users.noreply.github.com>
2024-10-21 21:40:49 -0300

    Add files via upload

---
+++ turma.js
@@ -0,0 +1,6 @@
+// Função que faz o cadastro funcionar
+function turma() {
+    let turma = document.getElementById('turma').value;
+    let ano_letivo = document.getElementById('ano-letivo').value;
+    let curso = document.getElementById('curso').value;
+}
